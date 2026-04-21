e57f8a2db3df5b2cc1299037bee70f91826cdc69
litvinenkodm1tiy <litvinenkodm1t@yandex.ru>
2026-04-21 16:45:06 +0300

    Создание сущности User и базовой конфигурации Hibernate (проведена проверка подключение к БД)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,36 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+
+            Properties props = new Properties();
+            try (InputStream input = Main.class.getClassLoader().getResourceAsStream("hibernate.properties")) {
+                if (input == null) {
+                    System.err.println("Файл hibernate.properties не найден в src/main/resources");
+                    return;
+                }
+                props.load(input);
+            }
+
+            Configuration cfg = new Configuration();
+            cfg.setProperties(props);
+            cfg.addAnnotatedClass(User.class);
+
+            SessionFactory factory = cfg.buildSessionFactory();
+            Session session = factory.openSession();
+            session.close();
+
+            System.out.println(" Подключение к БД успешно!");
+            factory.close();
+
+        } catch (Exception e) {
+            System.err.println(" Ошибка подключения:");
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,29 @@
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Integer age;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
