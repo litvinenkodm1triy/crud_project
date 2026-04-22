dc40b8ece283be8eaff25c094c2f5293fff01f89
litvinenkodm1tiy <litvinenkodm1t@yandex.ru>
2026-04-22 10:34:05 +0300

    Рефакторинг структуры проекта, добавлены слои: controller, dao, entities,exceptions, util, validation.

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import entities.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    void save(User user);
+
+    User findById(Long id);
+
+    List<User> findAll();
+
+    void update(User user);
+
+    void delete(Long id);
+
+
+}
